c687fe51ababa940cffb9be1760aa0c62642c7e6
bayendemba <bayendemba@gmail.com>
2017-06-10 15:13:45 +0000

    LOGIN CHANAGES

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-
-
 import { MyApp } from './app.component';
 //import { HomePage } from '../pages/home/home'; //Delete it Also like app.component.ts
 
